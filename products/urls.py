1f2286cc4c7ee580bfbecf0bf8d4c93fb500379f
worldofmarcus <me@marcuseriksson.com>
2022-10-26 14:47:13 +0000

    Add basic views and template in product app

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.list_all_products, name='list_all_products'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def list_all_products(request):
+    """ A view that lists all products """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+        }
+
+    return render(request, 'products/products.html', context)
