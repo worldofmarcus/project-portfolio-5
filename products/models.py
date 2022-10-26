b72319d83bd3ce09fd037b220e0eca28c6acf1e9
worldofmarcus <me@marcuseriksson.com>
2022-10-26 12:07:17 +0000

    Create product model in products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """ Product model """
+
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    tags = models.ManyToManyField(Tag)
+    description = models.TextField()
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    product_size = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
