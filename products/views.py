66fc2837045e415b968f4eb6619b43e3cb2d092a
worldofmarcus <me@marcuseriksson.com>
2022-10-28 07:56:39 +0000

    Add working pagination for both filtered and non filtered queries n products template and list_all_products view

--- products/views.py
+++ products/views.py
@@ -11,6 +11,7 @@ def list_all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
     category = None
 
     total_products = Product.objects.all().count()
@@ -18,6 +19,7 @@ def list_all_products(request):
     if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
+            category = request.GET['category']
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
@@ -31,19 +33,20 @@ def list_all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    # # Pagination code
-    # p = Paginator(products, 6)
-    # page = request.GET.get('page')
-    # product_list = p.get_page(page)
-    # # End of Pagination code
+    # Pagination code
+    p = Paginator(products, 6)
+    page = request.GET.get('page')
+    product_list = p.get_page(page)
+    # End of Pagination code
 
 
     context = {
         'total_products': total_products,
-        # 'product_list': product_list,
-        'products': products,
+        'product_list': product_list,
         'search_term': query,
         'current_categories': categories,
+        'category': category
+
         }
 
     return render(request, 'products/products.html', context)
