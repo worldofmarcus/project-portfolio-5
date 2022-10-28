5c5f16840669954b053b07ea18f1f89bea3a350c
worldofmarcus <me@marcuseriksson.com>
2022-10-28 06:37:26 +0000

    Add list by category functionality in products.html and view in product app

--- products/views.py
+++ products/views.py
@@ -11,30 +11,39 @@ def list_all_products(request):
 
     products = Product.objects.all()
     query = None
+    category = None
 
     total_products = Product.objects.all().count()
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You did not enter anything to search for!")
+                messages.error(request, 'You did not enter any search criteria!')
                 return redirect(reverse('list_all_products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    # Pagination code
-    p = Paginator(products, 6)
-    page = request.GET.get('page')
-    product_list = p.get_page(page)
-    # End of Pagination code
+    # # Pagination code
+    # p = Paginator(products, 6)
+    # page = request.GET.get('page')
+    # product_list = p.get_page(page)
+    # # End of Pagination code
 
 
     context = {
         'total_products': total_products,
-        'product_list': product_list,
-        'search_term': query
+        # 'product_list': product_list,
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
         }
 
     return render(request, 'products/products.html', context)
