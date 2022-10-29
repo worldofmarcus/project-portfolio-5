8328740707d9fa198da158701727b57872cba991
worldofmarcus <me@marcuseriksson.com>
2022-10-29 06:54:29 +0000

    Add product imt images and add product_detail teamplate and view

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list_all_products, name='list_all_products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -76,3 +76,15 @@ def list_all_products(request):
         }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view that lists a specific product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
