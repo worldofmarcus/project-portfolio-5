44add895f4302397b089eb6e4372df3774c28e3e
worldofmarcus <me@marcuseriksson.com>
2022-10-31 15:52:20 +0000

    Fix image no file attribute associated bug on product.html, product_details.html and bag.html and add automatic SKU generator in product model

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 
 class Tag(models.Model):
@@ -36,10 +37,11 @@ class Product(models.Model):
     tags = models.ManyToManyField(Tag)
     description = models.TextField()
     rating = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254, default=uuid.uuid4)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     product_size = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
