ad0c36216f35706a325963cdb4262158d49ddf0d
worldofmarcus <me@marcuseriksson.com>
2022-11-01 10:38:06 +0000

    Add basic HTML and JS-structure for updating cart in product_detail.html and bag.html and start doing some refactoring of code in the bag app

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
+
+from products.models import Product
 
 # Create your views here.
 
@@ -51,3 +56,43 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+            messages.success(request,
+                             (f'Updated size {size.upper()} '
+                              f'{product.name} quantity to '
+                              f'{bag[item_id]["items_by_size"][size]}'))
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+            messages.success(request,
+                             (f'Removed size {size.upper()} '
+                              f'{product.name} from your bag'))
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {bag[item_id]}'))
+        else:
+            bag.pop(item_id)
+            messages.success(request,
+                             (f'Removed {product.name} '
+                              f'from your bag'))
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
