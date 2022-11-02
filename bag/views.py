966bcb7b53c98ed7ba392bb14a66b57200e69817
worldofmarcus <me@marcuseriksson.com>
2022-11-02 16:36:00 +0000

    Add basic structure for toast bootstrap messages

--- bag/views.py
+++ bag/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.shortcuts import (
     render, redirect, reverse, HttpResponse, get_object_or_404
 )
-
+from django.contrib import messages
 from products.models import Product
 
 # Create your views here.
@@ -19,6 +17,7 @@ def add_to_bag(request, item_id):
     """ This view adds the quantity of a specific item
     to the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -30,32 +29,24 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                # messages.success(request,
-                #                  (f'Updated size {size.upper()} '
-                #                   f'{product.name} quantity to '
-                #                   f'{bag[item_id]["items_by_size"][size]}'))
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
-                # messages.success(request,
-                #                  (f'Added size {size.upper()} '
-                #                   f'{product.name} to your bag'))
+                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-            # messages.success(request,
-            #                  (f'Added size {size.upper()} '
-            #                   f'{product.name} to your bag'))
+            messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            # messages.success(request,
-            #                  (f'Updated {product.name} '
-            #                   f'quantity to {bag[item_id]}'))
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
-            # messages.success(request, f'Added {product.name} to your bag')
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
 
 
 def adjust_bag(request, item_id):
@@ -71,28 +62,18 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
-            # messages.success(request,
-            #                  (f'Updated size {size.upper()} '
-            #                   f'{product.name} quantity to '
-            #                   f'{bag[item_id]["items_by_size"][size]}'))
+
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            # messages.success(request,
-            #                  (f'Removed size {size.upper()} '
-            #                   f'{product.name} from your bag'))
+
     else:
         if quantity > 0:
             bag[item_id] = quantity
-            # messages.success(request,
-            #                  (f'Updated {product.name} '
-            #                   f'quantity to {bag[item_id]}'))
+
         else:
             bag.pop(item_id)
-            # messages.success(request,
-            #                  (f'Removed {product.name} '
-            #                   f'from your bag'))
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
