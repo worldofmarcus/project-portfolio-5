b2d00df52509bf5141575d1245dad3afcf0c33d5
worldofmarcus <me@marcuseriksson.com>
2022-11-07 20:42:38 +0000

    Create blog app and basic structure for blog models

---
+++ blog/models.py
@@ -0,0 +1,67 @@
+"""Import relevant modules for the application"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+class Category(models.Model):
+    """
+    This class creates the Category model.
+    """
+
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        """
+        This meta class orders the model by name.
+        """
+
+        ordering = ['name']
+
+    def __str__(self):
+        """
+        This function returns the name of the category
+        to add user readability.
+        """
+
+        return str(self.name)
+
+
+CATEGORY_CHOICES = Category.objects.all().values_list('name', 'name')
+choice_list = []
+for item in CATEGORY_CHOICES:
+    choice_list.append(item)
+
+
+class Post(models.Model):
+    """
+    This class creates the blog post model.
+    """
+
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="posts")
+    body = models.TextField()
+    category = models.CharField(max_length=255, default='Uncategorized',
+                                choices=choice_list)
+    status = models.IntegerField(choices=STATUS, default=0)
+    date_created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """
+        This meta class orders the model by date.
+        """
+
+        ordering = ['-date_created_on']
+
+    def __str__(self):
+        """
+        This function returns the title
+        to add better user readability.
+        """
+
+        return self.title
--- kollektiv_fem/settings.py
+++ kollektiv_fem/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'blog',
     'django_countries',
     'crispy_forms',
 ]
