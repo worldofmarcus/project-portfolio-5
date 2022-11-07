e06a55b1d7aea1a58e81e5cedaa799957690ecd1
worldofmarcus <me@marcuseriksson.com>
2022-11-07 14:36:13 +0000

    Add further functionality to add product form

--- products/views.py
+++ products/views.py
@@ -95,8 +95,17 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ This view adds a product to the site """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
 
-    form = ProductForm
     template = 'products/add_product.html'
     context = {
         'form': form,
--- profiles/views.py
+++ profiles/views.py
@@ -7,17 +7,20 @@ from checkout.models import Order
 
 
 def profile(request):
-    """This method displays the users profile"""
+    """ This view displays the users profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully!')
-
-    form = UserProfileForm(instance=profile)
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
         'form': form,
