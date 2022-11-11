2a6e3f4dabb0112c6862fd3b48d23c6e2baa772a
worldofmarcus <me@marcuseriksson.com>
2022-11-11 08:21:57 +0000

    Create stripe_elements.js and add stripe card field to checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -107,6 +107,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51M17ONFksvm22MvOKasJ5M2Fbi7I4DxskiJBFJRLFS0LmeGUi6zVHLbzEeq6pJ55TTDzTB4IHkbCptBZTOFOM7hH00AL8wEWiG',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
