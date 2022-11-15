484e9c01cb2acfbf21500a6797e8a014ba9604ed
worldofmarcus <me@marcuseriksson.com>
2022-11-15 12:48:11 +0000

    Add privacy policy on site

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from blog.views import list_all_posts, blog_post_detail
+from . import views
 
 urlpatterns = [
     path('', list_all_posts, name='home'),
+    path('privacy_policy', views.privacy_policy, name='privacy_policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-# def index(request):
-#     return render(request, 'home/index.html')
+def privacy_policy(request):
+    return render(request, 'home/privacy_policy.html')
