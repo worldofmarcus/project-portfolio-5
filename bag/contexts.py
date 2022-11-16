980c7f254e35e807c0e484009b2d64bea6792d96
worldofmarcus <me@marcuseriksson.com>
2022-11-16 17:18:56 +0000

    Tidy up code in contexts.py in bag app

--- bag/contexts.py
+++ bag/contexts.py
@@ -33,7 +33,6 @@ def bag_contents(request):
                     'size': size,
                 })
 
-
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
