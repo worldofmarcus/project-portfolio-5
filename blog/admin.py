4d9884c389cf4e4b5c74ed29a70dee683a69b000
worldofmarcus <me@marcuseriksson.com>
2022-11-16 17:59:07 +0000

    Tidy up code in admin.py in blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, Category
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     """
@@ -10,6 +11,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
     list_display = ('name', )
     list_filter = ('name', )
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
