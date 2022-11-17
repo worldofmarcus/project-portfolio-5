d71cd8ed942420b18adc00558e0a081d8070c254
worldofmarcus <me@marcuseriksson.com>
2022-11-17 15:41:33 +0000

    Add faq page (view, url and template) with link to it in footer

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', view_latest_blog_posts, name='home'),
-    path('privacy_policy', views.privacy_policy, name='privacy_policy'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('faq/', views.faq, name='faq'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def privacy_policy(request):
     return render(request, 'home/privacy_policy.html')
+
+def faq(request):
+    return render(request, 'home/faq.html')
+
